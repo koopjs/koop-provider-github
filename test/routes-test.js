034f81b3865d5558f55b892e737c8e2c1c7fb9e7
chelm <christopher.helm@gmail.com>
2014-04-21 14:08:11 -0600

    route testing pattern is HOT

--- test/routes-test.js
+++ test/routes-test.js
@@ -1,66 +1,81 @@
-var should = require('should');
-var path = require('path');
-var superagent = require('superagent');
+var should = require('should'),
+  request = require('supertest'),
+  config = require('config'),
+  koop = require('koop-server')(config);
+
+global.config = config;
 
 before(function (done) {
-    done();
+    Cache.db = PostGIS.connect( config.db.postgis.conn );
+    try { koop.register(require("../index.js")); } catch(e){ console.log('shit')}
+    done(); 
 });
 
 describe('Koop Routes', function(){
 
-    var agent = superagent.agent();
-
-    describe('/github', function() {
-      it('should return 404', function(done) {
-          agent.get('http://localhost:1337/github/').end(function(err, res) {
+    describe('/github/colemanm/hurricanes/fl_2004_hurricanes', function() {
+      it('should return 200', function(done) {
+        request(koop)
+          .get('/github/colemanm/hurricanes/fl_2004_hurricanes')
+          .end(function(err, res){
             res.should.have.status(200);
-            return done();
+            done();
           });
       });
     });
 
     describe('/github/blarg/', function() {
       it('should return 404', function(done) {
-          agent.get('http://localhost:1337/github/blarg/').end(function(err, res) {
+        request(koop)
+          .get('/github/blarg')
+          .end(function(err, res){
             res.should.have.status(404);
-            return done();
-          });
+            done();
+        });
       });
     });
 
     describe('/github/colemanm/hurricanes/fl_2004_hurricanes', function() {
       it('should return 200', function(done) {
-          agent.get('http://localhost:1337/github/colemanm/hurricanes/fl_2004_hurricanes').end(function(err, res) {
+        request(koop)
+          .get('/github/colemanm/hurricanes/fl_2004_hurricanes')
+          .end(function(err, res){
             res.should.have.status(200);
-            return done();
-          });
+            done();
+        });
       });
     });
 
     describe('/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer', function() {
       it('should return 200', function(done) {
-          agent.get('http://localhost:1337/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer').end(function(err, res) {
+        request(koop)
+          .get('/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer')
+          .end(function(err, res){
             res.should.have.status(200);
-            return done();
-          });
+            done();
+        });
       });
     });
 
     describe('/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer/0', function() {
       it('should return 200', function(done) {
-          agent.get('http://localhost:1337/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer/0').  end(function(err, res) {
+        request(koop)
+          .get('/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer/0')
+          .end(function(err, res){
             res.should.have.status(200);
-            return done();
-          });
+            done();
+        });
       });
     });
 
     describe('/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer/0/query', function() {
       it('should return 200', function(done) {
-          agent.get('http://localhost:1337/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer/0/query').end(function(err, res) {
+        request(koop)
+          .get('/github/colemanm/hurricanes/fl_2004_hurricanes/FeatureServer/0/query')
+          .end(function(err, res){
             res.should.have.status(200);
-            return done();
-          });
+            done();
+        });
       });
     });
 
