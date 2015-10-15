fc19f5d620f8d866522d244ad3f1433bf71be50d
Nate Goldman <nate@ngoldman.me>
2015-10-15 15:34:38 -0700

    :tv: improve example

--- example/index.js
+++ example/index.js
@@ -1,21 +1,29 @@
+var morgan = require('morgan')
+var errorHandler = require('errorhandler')
 var app = require('express')()
 var koop = require('koop')({
-  'db': {
-    'conn': 'postgres://localhost/koopdev'
-  },
   'ghtoken': process.env.KOOP_GITHUB_TOKEN
 })
 var github = require('../')
 
 koop.register(github)
 
+app.set('port', process.env.PORT || 1337)
 app.set('json spaces', 2)
+
+if (app.get('env') === 'production') {
+  app.use(morgan())
+} else {
+  app.use(morgan('dev'))
+  app.use(errorHandler())
+}
+
 app.use('/koop/', koop)
 
 app.get('/', function (req, res) {
   res.redirect('/koop/github')
 })
 
-app.listen(process.env.PORT || 1337, function () {
-  console.log('Koop Github example server listening at %d', this.address().port)
+app.listen(app.get('port'), function () {
+  console.log('koop-github example server listening at %d', this.address().port)
 })
